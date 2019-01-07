a67d92c1d7c6c94366031e222e81aef14e943ba0
k17rs066 <k17rs066@st.kyusan-u.ac.jp>
2019-01-07 13:52:42 +0900

    test2

---
+++ www/css/keys.js
@@ -0,0 +1,3 @@
+// This is a JavaScript file
+var appKey = "0aa0739d00a5a9326fcd48b23c28c78834b401288b2aa6c0696d24de1b116b4f";
+var clientKey = "cb8a6d5f4f8c4f0406740830d2aeeda1e31e9a6621c1a0ae7c9e6738e7740ffb";
